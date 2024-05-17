81a286dce1cff5d39f5ed96e8675d4fd5dc7c361
dungnt <mrsoi2302@gmail.com>
2024-05-17 20:55:54 +0700

    complete

--- src/main/java/com/example/jwt/LoginController.java
+++ src/main/java/com/example/jwt/LoginController.java
@@ -1,19 +1,22 @@
 package com.example.jwt;
 
-import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/login")
 @RequiredArgsConstructor
+@Slf4j
 public class LoginController {
 
     private final UserDetailsService userDetailsService;
 
     @PostMapping("/{alg}")
     public String login(@PathVariable String alg,@RequestBody User user) {
-        return userDetailsService.generateToken(alg,user);
+        var token = userDetailsService.generateToken(alg,user);
+        log.info("Key: {}", Key.Value);
+        return token;
     }
 
 }
